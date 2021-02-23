24f65c5f954fc8d45a12e63626e2d1ef4959d0de
pekorinko <doridori.program@gmail.com>
2021-02-23 10:48:41 +0900

    URLアクセス後、口コミ画面が表示されるまで待つ

--- no_number.rb
+++ no_number.rb
@@ -5,11 +5,16 @@ require 'pry'
 
 d = Selenium::WebDriver.for :chrome
 
-wait = Selenium::WebDriver::Wait.new(timeout: 100)
+wait = Selenium::WebDriver::Wait.new(timeout: 20)
 
-d.get('https://www.google.com/search?rlz=1C5CHFA_enJP939JP939&sxsrf=ALeKk03piFZiTXDS_S-GwnYVa4JmAemjuA%3A1613137898660&ei=6ocmYKfoJ4H3hwPrv4WYBg&q=%E6%B9%AF%E6%A5%BD%E3%81%AE%E9%87%8C%E3%80%80%E5%9B%BD%E7%AB%8B&oq=%E6%B9%AF%E6%A5%BD%E3%81%AE%E9%87%8C%E3%80%80%E5%9B%BD%E7%AB%8B&gs_lcp=Cgdnd3Mtd2l6EAMyBAgjECcyBAgjECcyAggAMgIIADICCAAyAggAMgIIADICCAA6BwgjELADECc6BwgAEEcQsAM6CAgAELEDEIMBOgQIABAEOgQIABAeOgYIABAIEB5QjhtYmSdg3CloAXACeACAAegBiAGdD5IBBTAuOC4zmAEAoAEBqgEHZ3dzLXdpesgBCsABAQ&sclient=gws-wiz&ved=0ahUKEwinhbH7vuTuAhWB-2EKHetfAWMQ4dUDCA0&uact=5#lrd=0x6018e3951c8fde6f:0x429b84dcae3b48f6,1,,,')
-sleep 5
-#binding.pry
+d.get('https://www.google.com/search?q=%E5%8D%97%E8%A1%8C%E5%BE%B3%20%E7%BE%8E%E5%AE%B9%E9%99%A2&rlz=1C5CHFA_enJP939JP939&oq=%E5%8D%97%E8%A1%8C%E5%BE%B3%E3%80%80%E7%BE%8E%E5%AE%B9%E9%99%A2&aqs=chrome.0.69i59j0l8.2958j0j9&sourceid=chrome&ie=UTF-8&tbs=lrf:!1m4!1u3!2m2!3m1!1e1!1m4!1u2!2m2!2m1!1e1!2m1!1e2!2m1!1e3,lf:1,lf_ui:2&tbm=lcl&sxsrf=ALeKk03-xdCjUMoqiGoGe6eTYQPopzxQNA:1614041099234&rflfq=1&num=10&rldimm=11505684056739296527&lqi=ChPljZfooYzlvrMg576O5a656ZmiWjAKFOWNl-ihjOW-syDnvo7lrrkg6ZmiIhTljZfooYzlvrMg576O5a65IOmZoioCCAOSAQpoYWlyX3NhbG9uqgESEAEqDiIK576O5a65IOmZoigI&ved=2ahUKEwjlnv3S4_7uAhWKHHAKHQgwCu0QvS4wAnoECAIQQQ&rlst=f#lrd=0x601887db5d7a5e6d:0x9fac66b09bd2350f,1,,,&rlfi=hd:;si:11505684056739296527,l,ChPljZfooYzlvrMg576O5a656ZmiWjAKFOWNl-ihjOW-syDnvo7lrrkg6ZmiIhTljZfooYzlvrMg576O5a65IOmZoioCCAOSAQpoYWlyX3NhbG9uqgESEAEqDiIK576O5a65IOmZoigI;mv:[[35.674233199999996,139.9075969],[35.6647782,139.8983212]];tbs:lrf:!1m4!1u3!2m2!3m1!1e1!1m4!1u2!2m2!2m1!1e1!2m1!1e2!2m1!1e3,lf:1,lf_ui:2')
+sleep 3
+
+binding.pry
+
+# wait.until { d.find_elements(:class_name, 'WMbnJf')[1].displayed? }
 #中身を待つのでもOKだが口コミページ全体が表示されるのを待つでも良いのでは無いか？
-wait.until { d.find_elements(:class_name, 'WMbnJf')[1].displayed? }
+wait.until { d.find_element(:class_name, 'lcorif').displayed? }
+
+p "hello"
 
